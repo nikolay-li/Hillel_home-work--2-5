3881a0f9ed6670c3759eb564d59ada183b2db0a1
Nikolay Li <koliahoy@gmail.com>
2022-10-20 17:58:43 +0300

    first commit

---
+++ src/scripts/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+
+alert('sum of 2 numbers');
+
+let number_a = prompt('number A');
+let number_b = prompt('number B');
+
+alert('sum = ' + (+number_a + +number_b));
+
+// alert(Number(number_a) + Number(number_b));
